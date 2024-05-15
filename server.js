3496c3f3167eb69ff5751ea2623a1fb7db79c3c8
Shalaka <shalakaw17@gmail.com>
2024-05-15 20:50:26 +0530

    cruds

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 require('dotenv').config()
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/1234", (req,res)=> {
-    res.send("Get all contacts");
-});
-
-app.post("/signup", (req,res) => {
-    // let {username,email,pwd}=req.body;
-    // res.send({
-    //     "message": "success",
-    //     "username": username,
-    //     "email": email,
-    //     "pwd": pwd
-    // })
-    console.log(req.body.username)
-})
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
